cc3fe2d89052ce7fb9755a7147e2b2fda34c3176
Emin <eminaydin93@yahoo.com>
2020-04-25 03:51:11 +0200

    Fetch api included

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const api = {
-  key:"674f174661d03bcc88281615e3d3871f",
-  base:"api.openweathermap.org/data/2.5/"
+  key: "674f174661d03bcc88281615e3d3871f",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
 
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result)
+          setQuery("");
+          console.log(result);
+          
+        });
+    }
+  }
+
+
   const dateBuilder = (d) => {
-    let months =  ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let days = ["Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday", "Sunday"];
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+      "Thursday", "Friday", "Saturday"];
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
@@ -18,31 +35,35 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
 
-  return ( 
-    <div className="app warm">
-    <main> 
-      <div className="search-box">
-        <input 
-        type="text"
-        className="search-bar"
-        placeholder="Search"
-        />
-      </div>
-      <div className="location-box">
-    <div className="location">
-New York City, US
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">
+            New York City, US
     </div>
-    <div className="date">
-{dateBuilder(new Date())}
-    </div>
-      </div>
-      <div className="weather-box">
-<div className="temp">
-15"c
+          <div className="date">
+            {dateBuilder(new Date())}
+          </div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            15"c
 </div>
-      </div>
-      <div className="weather"> Sunny </div>
-    </main>
+          <div className="weather"> Sunny </div>
+        </div>
+
+      </main>
     </div>
   );
 }
