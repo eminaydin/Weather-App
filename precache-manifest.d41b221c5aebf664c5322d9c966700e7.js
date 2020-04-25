07a079333a1de611df5ac6aa44cce5a9d5203bda
Emin <eminaydin93@yahoo.com>
2020-04-26 01:05:22 +0200

    Updates

---
+++ precache-manifest.d41b221c5aebf664c5322d9c966700e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f63d7ac2f7a8162060de41c68da258",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "2419ec94a680e481af64",
+    "url": "/Weather-App/static/css/main.fc251940.chunk.css"
+  },
+  {
+    "revision": "1438712fc7888e3c033b",
+    "url": "/Weather-App/static/js/2.44bafd64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.44bafd64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2419ec94a680e481af64",
+    "url": "/Weather-App/static/js/main.87a64559.chunk.js"
+  },
+  {
+    "revision": "1b51043f8cc13e8b9155",
+    "url": "/Weather-App/static/js/runtime-main.57dc0fb0.js"
+  }
+]);
